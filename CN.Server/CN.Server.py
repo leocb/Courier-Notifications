7b300701e8bb8a3d39a6df1918afea5a05e5e14f
leocb <leobottaro@gmail.com>
2022-02-10 18:15:14 -0300

    Update server var naming and some minor tweaks

--- CN.Server/CN.Server.py
+++ CN.Server/CN.Server.py
@@ -12,49 +12,60 @@ class MainHandler(tornado.web.RequestHandler):
         loader = tornado.template.Loader(".")
         self.write("Oh hi! What are you doing here? :)")
 
-        
-RoomClientListDict = {}
-ClientRoomDict = {}
+
+ChannelClientListDict = {}
+ClientChannelDict = {}
+
 
 class WSHandler(tornado.websocket.WebSocketHandler):
-
     def check_origin(client, origin):
         return True
 
-    def open(client, RoomID):
-        if RoomID is None:
-            client.write_message("room argument is missing")
-            client.close();
+    def open(client, ChannelID):
+        if ChannelID is None or not ChannelID:
+            client.write_message("Error: Channel argument is missing")
+            print("A client tried to connected without a Channel arg")
+            client.close()
+            return
+
+        ChannelID = ChannelID.upper()
+        ClientChannelDict[client] = ChannelID
+
+        if ChannelID in ChannelClientListDict:
+            ChannelClientListDict[ChannelID].append(client)
         else:
-            RoomID = RoomID.upper()
+            ChannelClientListDict[ChannelID] = [client]
 
-            ClientRoomDict[client] = RoomID
-            
-            if RoomID in RoomClientListDict:
-                RoomClientListDict[RoomID].append(client)
-            else:
-                RoomClientListDict[RoomID] = [client]
-
-            print('client connected to ' + RoomID)
+        print("A client connected to " + ChannelID)
 
     def on_message(client, message):
-        print(ClientRoomDict[client] + '|' + message)
-        EchoList = RoomClientListDict[ClientRoomDict[client]]
-        for v in EchoList:
-            v.write_message(message)
+        try:
+            ChannelID = ClientChannelDict[client]
+            print(ChannelID + " | " + message)
+            EchoList = ChannelClientListDict[ChannelID]
+            for c in EchoList:
+                c.write_message(message)
+        except:
+            pass
 
     def on_close(client):
-        RoomClientListDict[ClientRoomDict[client]].remove(client)
-        del ClientRoomDict[client]
-        print('client disconnected')
+        try:
+            ChannelID = ClientChannelDict[client]
+            ChannelClientListDict[ChannelID].remove(client)
+            del ClientChannelDict[client]
+            print("A client disconnected from " + ChannelID)
+        except:
+            pass
 
 
-application = tornado.web.Application([
-    (r'/ws/(.*)', WSHandler),
-    (r'/', MainHandler),
-])
+application = tornado.web.Application(
+    [
+        (r"/ws/(.*)", WSHandler),
+        (r"/", MainHandler),
+    ]
+)
 
 if __name__ == "__main__":
-    print('ready!')
+    print("Ready!")
     application.listen(9091)
     tornado.ioloop.IOLoop.instance().start()
