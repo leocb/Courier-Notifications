318db2e2b98a2d11e05ce00a3b69274a2a49bf9d
Leo Bottaro <github@leobottaro.com>
2024-02-01 22:34:58 -0300

    Update Message Model to include sender ID

--- CN.Models/Messages/Message.cs
+++ CN.Models/Messages/Message.cs
@@ -7,9 +7,9 @@ public class Message
 {
     //public Guid Center { get; set; }
     //public Guid Space { get; set; }
-    //public Guid From { get; set; }
     public DateTime Date { get; set; }
-    public string Text { get; set; } = string.Empty;
-    public string FromName { get; set; } = string.Empty;
     public MessageStatus Status { get; set; } = MessageStatus.None;
+    public Guid FromKey { get; set; }
+    public string FromName { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
 }
