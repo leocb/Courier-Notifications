5f8f78b799a5c2e8f079dd34c8266a7cd262bc49
Leo Bottaro <github@leobottaro.com>
2024-09-13 00:04:42 -0300

    wait

--- CN.Web.App/build.sh
+++ CN.Web.App/build.sh
@@ -6,4 +6,5 @@ chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
 ./dotnet/dotnet publish -c Release -o ./publish &
+wait
 echo $'"{"ApiHostname": "$1"}' > publish/wwwroot/appsettings.json
