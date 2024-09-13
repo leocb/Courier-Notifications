fb7928bc1f3eaa10a96bac284991c3f67982dc05
Leo Bottaro <github@leobottaro.com>
2024-09-12 22:52:27 -0300

    update build.sh

--- CN.Web.App/build.sh
+++ CN.Web.App/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+pwd
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
-./dotnet/dotnet publish -c Release -o ./publish
-echo $'"{"ApiHostname": "$1"}' > ./publish/wwwroot/appsettings.json
+./dotnet/dotnet publish -c Release -o ./publish &
+wait
+echo $'"{"ApiHostname": "$1"}' > publish/wwwroot/appsettings.json
