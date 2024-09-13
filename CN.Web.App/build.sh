ec82f3026e0b24dd6347ad76a87ffe1405054dbe
Leo Bottaro <github@leobottaro.com>
2024-09-12 22:34:52 -0300

    Delete CN.Web.App/build.sh

--- CN.Web.App/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
-./dotnet/dotnet --version
-./dotnet/dotnet publish -c Release -o ./publish
-echo $'"{"ApiHostname": "$1"}' > ./publish/wwwroot/appsettings.json
