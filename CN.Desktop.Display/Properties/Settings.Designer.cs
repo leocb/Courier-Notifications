1ecebb8d9bb5d498810631eb61de0cb83c1a27ab
Leo Bottaro <github@leobottaro.com>
2024-10-27 17:44:07 -0300

    Publish V1.0.2

--- CN.Desktop.Display/Properties/Settings.Designer.cs
+++ CN.Desktop.Display/Properties/Settings.Designer.cs
@@ -180,5 +180,17 @@ namespace CN.Desktop.Display.Properties {
                 this["FrontUrl"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool UpgradeRequired {
+            get {
+                return ((bool)(this["UpgradeRequired"]));
+            }
+            set {
+                this["UpgradeRequired"] = value;
+            }
+        }
     }
 }
